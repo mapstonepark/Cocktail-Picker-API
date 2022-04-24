612e9566c90fc1dc47a3116e9805a4f4aada3a36
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2022-04-24 17:28:29 -0500

    revert

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
-const apikey="563492ad6f91700001000001ec0b7a4d36a84336820a871d57fcef4b";
-document.querySelector('button').addEventListener('click', runMe);
+// The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
+document.querySelector('button').addEventListener('click', getDrink)
 
+function getDrink(){
 
-function runMe(){
-    let search = document.querySelector('input').value
-    fetch(`https://api.pexels.com/v1/search?query=${search}`)
-        .then(res=> res.json())
-        .then(data=>{
-            console.log(data.photos[0])
-            document.querySelector('img').src = data.photos[0].src.medium
-            document.querySelector('h2').innerText = data.photos[0].photographer
-            document.querySelector('.photographerUrl').innerText = data.photos[0].photographer_url
-        })
-        .catch(err=>{
-            console.log(`error ${err}`)
-        })
+    let drink = document.querySelector('input').value;
 
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+.then(res => res.json())
+.then(data => {
+    console.table(data.drinks[0])
+    document.querySelector('h2').innerText= data.drinks[0].strDrink
+    document.querySelector('h3').innerText= data.drinks[0].strInstructions
+    document.querySelector('img').src= data.drinks[0].strDrinkThumb
+    document.querySelector('p').innerText= `Here's what you need for your drink:  ${data.drinks[0].strMeasure1}, ${data.drinks[0].strMeasure2}, ${data.drinks[0].strMeasure3} `
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
 }
+
+
+// search for cocktail in api list of coctails and retrieve just the one user enters
+
+// post name, image and recipe on the page
